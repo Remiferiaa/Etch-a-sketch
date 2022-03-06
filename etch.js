7e3eea89c226c0bbfb0b057ae9b6bd54c7c76693
Remiferiaa <slong819@hotmail.com>
2022-03-06 22:20:10 +0800

    Prompt cancel fix and unused eventlistener removal

--- etch.js
+++ etch.js
@@ -1,4 +1,7 @@
-function grid(size) {
+let col = "black"
+let size = 16
+
+function grid() {
     document.getElementById("clear").click()
     for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
@@ -13,10 +16,17 @@ function grid(size) {
     };
 }
 
+function brushCol() {
+    if (col == "random") {
+        return rgbRand();
+    } 
+    return col;
+}
+
 function hover() {
     document.querySelectorAll(".square").forEach(function (square) {
         square.addEventListener("mouseover", function () {
-            square.style.backgroundColor = "black"
+            square.style.backgroundColor = brushCol();
         });
     });
 }
@@ -29,14 +39,9 @@ function rgbRand() {
     return "rgb(" + color.join(",") + ")"
 }
 
-
 function erase() {
     document.getElementById("erase").addEventListener("click", function () {
-        document.querySelectorAll(".square").forEach(function (square) {
-            square.addEventListener("mouseover", function () {
-                this.style.backgroundColor = "white";
-            });
-        });
+        col = "white";
     });
 }
 
@@ -59,33 +64,33 @@ function gridSize() {
     document.getElementById("size").addEventListener("click", function () {
         let input = prompt("specify the grid size you prefer with a maximum of 100");
         while ((isNaN(input)) || input == "" || input % 1 != 0 || input < 16 || input > 100) {
+            if (input == null) return;
             alert("invalid input or out of range");
             input = prompt("specify the grid size you prefer with a maximum of 100");
             if (false) break;
         }
         reset();
-        grid(input);
+        size = input;
+        grid();
         hover();
     });
 }
 
 function defColor() {
     document.getElementById("default").addEventListener("click", function () {
-        hover();
+        col = "black"
     });
 }
 
 function randColor() {
     document.getElementById("rainbow").addEventListener("click", function () {
         document.querySelectorAll(".square").forEach(function (square) {
-            square.addEventListener("mouseover", function () {
-                square.style.backgroundColor = rgbRand();
-            });
+            col = "random";
         });
     });
 }
 
-grid(16);
+grid();
 hover();
 erase();
 clear();
